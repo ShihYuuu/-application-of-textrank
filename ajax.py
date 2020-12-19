a7509632eb385e01ec5ea6ae6125d1963bbc7741
Wang Shih Yu <67854884+ShihYuuu@users.noreply.github.com>
2020-12-20 02:05:44 +0800

    Update ajax.py

--- ajax.py
+++ ajax.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('ajax.html')
+    return render_template('HomePage.html')
 
 @app.route('/getjob/')
 def getjob():
